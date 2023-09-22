0be4a087152bf0601c375300ac23db8aa32bface
Escandon <aleskndon@hotmail.com>
2023-09-22 00:56:15 -0600

    start

---
+++ database/migrations/2023_09_22_065024_create_user_domicilios_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserDomiciliosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_domicilios', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_id')->primary();
+            $table->string('domicilio');
+            $table->int('numero_exterior');
+            $table->string('colonia');
+            $table->string('cp');
+            $table->string('ciudad');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_domicilios');
+    }
+}
